a7285abf1e9e9feff59740d3efab58fa789e2bfa
riririusei99 <riry1313@gmail.com>
2018-01-12 21:01:07 +0900

    change spec

--- specs/spec.js
+++ specs/spec.js
@@ -2,6 +2,7 @@
 describe('My First Test', function() {
   it('Search riririusei99', function() {
     browser.get('http://teamspirit.hatenablog.com/');
+    
     element(by.css('input[type=text')).sendKeys('riririusei99');
     element(by.css('input[type=submit]')).click();
 
